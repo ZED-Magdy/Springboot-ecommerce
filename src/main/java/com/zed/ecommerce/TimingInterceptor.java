de3760d538e01908e85d8e78d811dbb3bcbaadba
ZED-Magdy <zedmagdy@gmail.com>
2025-07-25 19:12:19 +0300

    Add TimingInterceptor for request timing and logging

---
+++ src/main/java/com/zed/ecommerce/TimingInterceptor.java
@@ -0,0 +1,38 @@
+package com.zed.ecommerce;
+
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import org.springframework.stereotype.Component;
+import org.springframework.web.servlet.HandlerInterceptor;
+import org.springframework.web.servlet.ModelAndView;
+
+@Component
+public class TimingInterceptor implements HandlerInterceptor {
+
+    @Override
+    public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) throws Exception {
+        long startTime = System.currentTimeMillis();
+        request.setAttribute("startTime", startTime);
+        System.out.println("Request URL: " + request.getRequestURL());
+        System.out.println("Request Method: " + request.getMethod());
+        System.out.println("Start Time: " + startTime);
+        return HandlerInterceptor.super.preHandle(request, response, handler);
+    }
+
+    @Override
+    public void postHandle(HttpServletRequest request, HttpServletResponse response, Object handler, ModelAndView modelAndView) throws Exception {
+        long startTime = System.currentTimeMillis();
+        long endTime = (Long) request.getAttribute("startTime");
+        long duration = startTime - endTime;
+        System.out.println("Request URL: " + request.getRequestURL());
+        System.out.println("Request Method: " + request.getMethod());
+        System.out.println("Duration: " + duration + " ms");
+        HandlerInterceptor.super.postHandle(request, response, handler, modelAndView);
+    }
+
+    @Override
+    public void afterCompletion(HttpServletRequest request, HttpServletResponse response, Object handler, Exception ex) throws Exception {
+        System.out.println("Request URL: " + request.getRequestURL());
+        HandlerInterceptor.super.afterCompletion(request, response, handler, ex);
+    }
+}
